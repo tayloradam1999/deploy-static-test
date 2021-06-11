c7171458944efd629c177d777690b253e1e9b212
tayloradam1999 <tayloradam1999@gmail.com>
2021-06-11 22:20:57 +0000

    Pushing file for task 2

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+""" Fabric File """
+
+from fabric.api import local, env, put, run
+from datetime import datetime
+import os.path
+
+env.hosts = ['34.73.206.41' , '35.175.196.34']
+env.passwords = {vagrant}
+
+def do_deploy(archive_path):
+    """ Deploys our web_static archive """
+
+    try:
+        if not os.path.exists(archive_path):
+            return False
+
+        archiveName = archive_path[9:]
+        archiveNameWithoutExtension = archiveName[:-4]
+
+        put(archive_path, '/tmp/' + archiveName)
+        run("mkdir -p /data/web_static/releases/" + archiveNameWithoutExtension)
+        run("tar -xzvf /tmp/" + archiveName + " -C " + "/data/web_static/releases/" + archiveNameWithoutExtension + " --strip-components") 
+        run("rm -f /tmp/" + archiveName)
+        run("rm -f /data/web_static/current")
+        run ("sudo ln -sf /data/web_static/releases/" + archiveNameWithoutExtension + " /data/web_static/current")
+        
+        return True
+    except:
+        return False
+
+
+def do_pack():
+    """ Pack up our web_static """
+
+    try:
+        now = datetime.now()
+
+        tarArchiveName = "web_static_" + now.strftime("%Y%m%d%H%M%S") + ".tgz"
+        tarArchivePath = "versions/" + tarArchiveName
+        
+        local("mkdir -p versions")
+
+        local("tar -czvf " + tarArchivePath + " web_static")
+
+        return tarArchivePath
+    except:
+        return None
